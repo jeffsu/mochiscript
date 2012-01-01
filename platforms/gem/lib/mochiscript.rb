c76d14b0e348f3810a350685fb6c028aae77460f
agate <agate.hao@gmail.com>
2012-01-01 15:59:04 +0800

    refacted npm/gem builder

--- platforms/gem/lib/mochiscript.rb
+++ platforms/gem/lib/mochiscript.rb
@@ -1,7 +1,3 @@
-module Mochiscript
-  VERSION = "0.4.3".sub("-", '.')
-end
-
+require File.dirname(__FILE__) + '/mochiscript/version'
 require File.dirname(__FILE__) + '/mochiscript/core'
 require File.dirname(__FILE__) + '/mochiscript/rails/engine' if defined?(::Rails)
-
